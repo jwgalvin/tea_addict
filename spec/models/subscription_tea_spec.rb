ac8a578b665e4e8e53a4d3450fc26fd278ee11f3
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-01 14:17:48 -0600

    tea & subscription_tea models and tests

--- spec/models/subscription_tea_spec.rb
+++ spec/models/subscription_tea_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe SubscriptionTea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :subscription }
+  it { should belong_to :tea }
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :description }
+  it { should validate_presence_of :temperature }
+  it { should validate_presence_of :brew_time }
+  it { should have_many(:subscription_teas) }
+  it { should have_many(:subscriptions).through(:subscription_teas) }
 end
