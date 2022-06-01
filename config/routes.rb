c64fe4d0d610e44274ca7c0559b0e6ef9f13f4ab
Joseph W Galvin <jwgalvin@gmail.com>
2022-06-01 10:12:02 -0600

    add spec and DB schema

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
       resources :tea, only: %i[show]
       resources :subscription, only: %i[create, edit]
     end
+  end
 end
---
+++ spec/factories/subscriptions.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :subscription do
+    title { "MyString" }
+    customer { nil }
+  end
+end
